d4ed04ff2eeaa9de27957faa16917803e2f65a19
Guilherme Moretti <gmoretti@thoughtworks.com>
2016-11-20 03:03:38 -0200

    Update HTTPotion version

--- mix.exs
+++ mix.exs
@@ -31,7 +31,7 @@ defmodule Crawler.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:httpotion, "~> 3.0.0"},
+      {:httpotion, "~> 3.0.2"},
       {:floki, "~> 0.11"},
       {:hound, "~> 1.0"}
     ]
